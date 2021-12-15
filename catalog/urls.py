4e9f2f96e3ce8df2a72087ecd2be066f35b25e7e
jerellynsia0428 <jerellynsia0428@gmail.com>
2021-12-15 12:06:19 +0800

    finish 8(no challenge borrowed)

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,3 +4,12 @@ from . import views
 urlpatterns = [
     path('',views.home)
 ]
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
+]
+urlpatterns += [
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+]
